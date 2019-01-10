ca7467b3b8246cbc3407bf81921187875abef634
Joe Bidinger <johnbidinger@gmail.com>
2019-01-10 12:41:28 -0500

    user class updated and passed SQL creation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'y337kGcys&zP3B'
@@ -14,11 +14,26 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(10000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner   
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
@@ -41,6 +56,7 @@ def newpost():
     if request.method == 'POST':
         blog_title = request.form['blog_title']
         body = request.form['body']
+        #TODO owner_id property of Blog class
         if empty_post(blog_title) and empty_post(body):
             # if both title and body have content go here
             new_post = Blog(blog_title, body)
